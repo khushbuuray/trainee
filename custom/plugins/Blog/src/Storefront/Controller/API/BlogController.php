17f28b49767491331294842134fb901ca2b23d2c
khushbu ray <khushbu.ray@icreativetechnolabs.com>
2025-05-22 11:12:45 +0530

    create API or Blog Module

---
+++ custom/plugins/Blog/src/Storefront/Controller/API/BlogController.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+namespace Blog\Storefront\Controller\API;
+
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\Routing\Annotation\RouteScope;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Shopware\Storefront\Controller\StorefrontController;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route(defaults: ['_routeScope' => ['store-api']])]
+
+class BlogController extends StorefrontController
+{
+    protected EntityRepository $blogRepository;
+    public function __construct(EntityRepository $blogRepository)
+    {
+        $this->blogRepository = $blogRepository;
+    } 
+
+   #[Route(path: '/store-api/blog-list', name: 'store-api.blog.list', methods: ['GET'])]
+
+    public function index(SalesChannelContext $salesChannelContext)
+    {
+         $criteria = new Criteria();
+        $result = $this->blogRepository->search($criteria, $salesChannelContext->getContext());
+        return $this->json($result->getEntities());
+    }
+
+    #[Route(path: '/store-api/blog-detail/{id}', name: 'store-api.blog.detail', methods: ['GET'])]
+    public function detail(string $id, SalesChannelContext $salesChannelContext)
+    {
+        $criteria = new Criteria([$id]);
+        $blog = $this->blogRepository->search($criteria, $salesChannelContext->getContext());
+         if (!$blog) {
+        return $this->json(['error' => 'Blog not found'], 404);
+    }        
+        return $this->json($blog);
+    }
+}
