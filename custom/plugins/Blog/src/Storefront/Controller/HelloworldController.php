0198cb8aa04d48c42f811e0d2539bbf9c16ba542
khushbu ray <khushbu.ray@icreativetechnolabs.com>
2025-05-22 12:04:00 +0530

    fetch data in storefront page

--- custom/plugins/Blog/src/Storefront/Controller/HelloworldController.php
+++ custom/plugins/Blog/src/Storefront/Controller/HelloworldController.php
@@ -2,6 +2,10 @@
 
 namespace Blog\Storefront\Controller;
 
+use Dom\Entity;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Storefront\Controller\StorefrontController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route; 
@@ -12,16 +16,26 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HelloworldController extends StorefrontController
 { 
+    protected EntityRepository $blogRepository;
+
+    public function __construct(EntityRepository $blogRepository)
+    {
+        $this->blogRepository = $blogRepository;
+    }
  
    #[Route(
         path: '/helloworld',
         name: 'frontend.helloworld.helloworld',
         methods: ['GET']
     )]
-    public function index(): Response
+    public function index(SalesChannelContext $salesChannelContext): Response
     {
+        $criteria = new Criteria();
+        $criteria->addAssociation('blogCategories');
+        $fetchBlog = $this->blogRepository->search($criteria, $salesChannelContext->getContext());
+        // dd($fetchBlog->getEntities());
         return $this->renderStorefront('@Blog/storefront/page/helloworld.html.twig',[
-            'example'=>'Hello world'
+            'blogList'=>$fetchBlog->getEntities()
         ]);
     }
 }
